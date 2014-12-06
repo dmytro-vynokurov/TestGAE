9a4a8855d99920311a585b317779261342949bff
dvinokurov <dvinokurov777@gmail.com>
2014-12-06 22:30:49 +0200

    created post method handler

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,13 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import cgi
+import urllib
+from google.appengine.ext import ndb
 import webapp2
+
+coordinate_key = ndb.Key('Coordinate', 'default_coordinate')
+
+
+class Coordinate(ndb.Model):
+    longitude = ndb.IntegerProperty(indexed=False)
+    latitude = ndb.IntegerProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello world! I am testing GAE')
 
+    def post(self):
+        coordinate = Coordinate(parent=coordinate_key)
+        print self.request.params
+        coordinate.latitude = int(self.request.get('latitude'))
+        coordinate.longitude = int(self.request.get('longitude'))
+        coordinate.put()
+        self.redirect('/')
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', MainHandler)
+                              ], debug=True)
